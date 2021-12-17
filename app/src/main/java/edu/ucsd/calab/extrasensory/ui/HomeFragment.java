3b080467dac8be9c462ed91119a124d0777d5a29
khangdang4vn <khang.dang.vn4@gmail.com>
2021-12-17 18:41:59 -0500

    init commit

---
+++ app/src/main/java/edu/ucsd/calab/extrasensory/ui/HomeFragment.java
@@ -0,0 +1,152 @@
+package edu.ucsd.calab.extrasensory.ui;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import edu.ucsd.calab.extrasensory.ESApplication;
+import edu.ucsd.calab.extrasensory.R;
+import edu.ucsd.calab.extrasensory.network.ESNetworkAccessor;
+
+/**
+ * This class is for the "home page", which acts as a dashboard for the user to know what is going on and to help debug problems.
+ *
+ * ========================================
+ * The ExtraSensory App
+ * @author Yonatan Vaizman yvaizman@ucsd.edu
+ * Please see ExtraSensory App website for details and citation requirements:
+ * http://extrasensory.ucsd.edu/ExtraSensoryApp
+ * ========================================
+ */
+public class HomeFragment extends BaseTabFragment {
+
+    private static final String LOG_TAG = "[ES-HomeFragment]";
+    private static final String NO_AVAILABLE_NETWORK_FOR_SENDING = "There's no available network now to send the data to the server.";
+    private static final String ALERT_BUTTON_TEXT_OK = "o.k.";
+
+    private ESApplication getESApplication()  {
+        MainActivity mainActivity = (MainActivity) getActivity();
+        return (ESApplication)mainActivity.getApplication();
+    }
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    private RadioGroup _dataCollectionRadioGroup = null;
+    private TextView _storedExamplesCount = null;
+    private TextView _feedbackQueueCount = null;
+//    private ImageView _watchIcon = null;
+    private Button _sendStoredExamplesButton = null;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View homeView = inflater.inflate(R.layout.fragment_home, container, false);
+        _dataCollectionRadioGroup = (RadioGroup)homeView.findViewById(R.id.radio_group_data_collection);
+        _dataCollectionRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio_data_collection_off:
+                        Log.i(LOG_TAG,"User turned data collection off");
+                        getESApplication().set_userSelectedDataCollectionOn(false);
+                        break;
+                    case R.id.radio_data_collection_on:
+                        Log.i(LOG_TAG,"User turned data collection on");
+                        getESApplication().set_userSelectedDataCollectionOn(true);
+                        break;
+                    default:
+                        Log.e(LOG_TAG,"Unrecognized action for data collection radio button group: " + checkedId);
+                        // Do nothing
+                }
+            }
+        });
+
+        _storedExamplesCount = (TextView)homeView.findViewById(R.id.text_zip_file_count);
+        presentNumStoredExamples();
+
+        _feedbackQueueCount = (TextView)homeView.findViewById(R.id.text_feedback_count);
+        presentFeedbackQueueCount();
+
+        _sendStoredExamplesButton = (Button)homeView.findViewById(R.id.button_send_stored_examples);
+        _sendStoredExamplesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!ESNetworkAccessor.getESNetworkAccessor().canWeUseNetworkNow()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setIcon(R.drawable.ic_launcher).setMessage(NO_AVAILABLE_NETWORK_FOR_SENDING);
+                    builder.setPositiveButton(ALERT_BUTTON_TEXT_OK,new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.create().show();
+
+                    return;
+                }
+                // If we can use network to send the data, go for it:
+                ESNetworkAccessor.getESNetworkAccessor().uploadWhatYouHave();
+            }
+        });
+
+        return homeView;
+    }
+
+
+    private void presentNumStoredExamples() {
+        int num = ESNetworkAccessor.getESNetworkAccessor().uploadQueueSize();
+        _storedExamplesCount.setText("" + num);
+    }
+
+    private void presentFeedbackQueueCount() {
+        int num = ESNetworkAccessor.getESNetworkAccessor().feedbackQueueSize();
+        _feedbackQueueCount.setText("" + num);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (_dataCollectionRadioGroup == null) {
+            Log.e(LOG_TAG,"radio group of data collection is null");
+        }
+        if (getESApplication().is_userSelectedDataCollectionOn()) {
+            _dataCollectionRadioGroup.check(R.id.radio_data_collection_on);
+        }
+        else {
+            _dataCollectionRadioGroup.check(R.id.radio_data_collection_off);
+        }
+    }
+
+    @Override
+    protected void reactToRecordsUpdatedEvent() {
+        super.reactToRecordsUpdatedEvent();
+        Log.d(LOG_TAG,"reacting to records-update");
+        //TODO: redraw the relevant image to the latest activity
+    }
+
+    @Override
+    protected void reactToNetworkQueueSizeChangedEvent() {
+        super.reactToNetworkQueueSizeChangedEvent();
+        presentNumStoredExamples();
+    }
+
+    @Override
+    protected void reactToFeedbackQueueSizeChangedEvent() {
+        super.reactToFeedbackQueueSizeChangedEvent();
+        presentFeedbackQueueCount();
+    }
+
+
+}
